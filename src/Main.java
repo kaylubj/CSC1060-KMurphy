c3c42368684fc012530d63304a6f8bea95d647bc
Kaylub M <kaylub.murphy.contact@gmail.com>
2025-02-20 06:42:54 -0700

    Fixed ending, just need to add comments

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static int bottlesOfBeer(int j) {
         String plural = (j == 1) ? "bottle" : "bottles";
-        String plural2 = (j==2)? "bottle": "bottles";
+        String plural2 = (j == 2) ? "bottle" : "bottles";
         System.out.println(j + " " + plural + " of beer on the wall, " + j + " " + plural + " of beer!"
                 + " Take one down, pass it around, " + --j + " " + plural2 + " of beer on the wall!");
         return j;
@@ -11,11 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Give me a number: ");
+        System.out.print("Give me a number: ");
         int beerBottles = input.nextInt();
+        int beerBottlesStart = beerBottles;
 
         while (beerBottles > 0) {
             beerBottles = bottlesOfBeer(beerBottles);
         }
+
+        String isPlural = (beerBottlesStart == 1) ? "bottle" : "bottles";
+        System.out.println("Go to the store and buy some more, " + beerBottlesStart
+                + " " + isPlural + " of beer on the wall!");
     }
 }
