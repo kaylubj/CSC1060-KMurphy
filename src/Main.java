9d18386aec155738a45e17f59899c268d603cb92
Kaylub M <kaylub.murphy.contact@gmail.com>
2025-02-21 10:55:57 -0700

    Finished program

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,65 @@
+/*
+Kaylub Murphy
+2/21/2025
+Purpose: Write a program that calculates the sum of the first 1 natural numbers,
+first using a for loop, then using a while loop,
+Prints out the first 15 numbers of a "fibonacci" sequence, if the first two numbers
+were the numbers they entered instead of 0 and 1,
+Finally takes a number from the user and counts up from that number in a
+"right triangle" pattern
+ */
+
 import java.util.Scanner;
 
 public class Main {
+
+    //The method for finding the sum of the first 10 numbers using a for loop
+    //int x is the toTen value (1) from the main method.
     public static void sumOfForNumbers(int x) {
+        //loopCounter counts the number of times the for loop has gone for
         int loopCounter;
+        //Create a variable for the number that x will be added to
         int y = 2;
-        x = 1;
-        for (loopCounter = 0; loopCounter <= 9; loopCounter++) {
+
+        /*The loop that will repeat. The loop counter starts at zero, and
+        each time the loop repeats the counter increases by 1. The loop will
+        run 10 times.*/
+        for (loopCounter = 1; loopCounter < 10; loopCounter++) {
+            //Sets the variable sum to be the sum of x and y
             int sum = (x + y);
             System.out.print(x + " + " + y + " = " + sum + "\n");
+            //Sets x to the new sum
             x = sum;
+            //Sets y to the next number in the 1-10 sequence (1,2,3,etc.)
             y = y + 1;
         }
     }
 
-    public static void sumOfWhileNumbers(int x) {
-        x = -1;
+    //The method for finding the sum of the first 10 numbers using a while loop
+    public static void sumOfWhileNumbers() {
+        //x is set to 1, y is set to 2, and counter is set to 1. These are the initial
+        //values these variables will enter with, and they will change within the while loop
+        int x = 1;
         int y = 2;
-        int sum = x + y;
-        int counter = 0;
+        int counter = 1;
 
-        while (counter >= 0 && counter < 10) {
-            System.out.println((sum) + " + " + y + " = " + (sum + y));
-            x = sum;
-            sum = (sum + y);
+        while (counter >= 1 && counter < 10) {
+            System.out.println((x) + " + " + y + " = " + (x + y));
+            //Runs calculations to add the first 10 natural numbers:
+            x = (x + y);
             y = y + 1;
             counter = counter + 1;
         }
     }
 
+    /*
+    Accepts fibonacciNum1 and fibonacciNum2 sent from the main method and sets x = the first
+    and y = the second
+     */
     public static void fibonacciMethod(int x, int y) {
-        int counter = 0;
-        while (counter <= 6) {
+        int counter = 1;
+        //Loop repeats 14 times. It is written as 7, but each loop displays 2 numbers at a time
+        while (counter <= 7) {
             System.out.print(x + " " + y + " ");
             x = x + y;
             y = x + y;
@@ -38,25 +67,30 @@ public class Main {
             counterPlusOne = counter + 1;
             counter = counterPlusOne;
         }
+        //Loop is broken after the 14th number, so this prints out number 15
         System.out.print(x);
     }
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        //Sends a value of 1 to the method sumOfForNumbers
         int toTen = 1;
         System.out.println("Let's add up the first 10 numbers:");
         sumOfForNumbers(toTen);
 
+        //Runs sumOfWhileNumbers
         System.out.println("\nLet's go again:");
-        sumOfWhileNumbers(toTen);
+        sumOfWhileNumbers();
 
+        //Takes the input of 2 numbers and sends them to fibonacciMethod
         System.out.print("\nEnter a number: ");
         int fibonacciNum1 = input.nextInt();
         System.out.print("Enter another: ");
         int fibonacciNum2 = input.nextInt();
         fibonacciMethod(fibonacciNum1, fibonacciNum2);
 
+        //Runs the method getUserNumber from the RightAngleLoop class
         RightAngleLoop.getUserNumber();
     }
 }
--- src/RightAngleLoop.java
+++ src/RightAngleLoop.java
@@ -4,13 +4,22 @@ public class RightAngleLoop {
     public static void getUserNumber() {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("\nFinally, enter one more number: ");
+        System.out.print("\n\nFinally, enter one more number: ");
         int rightAngle = input.nextInt();
-        int firstNumber = 0;
+        int n;
+        //Starts the first value as an empty character, that will change in the loop
         String firstLine = "";
-        for (firstNumber = 0; firstNumber <= 9; firstNumber++) {
-            String nextLines = firstLine + " " + (rightAngle + firstNumber);
+        //Loop repeats 10 times, starting at 0 and counting up to 9
+        //n increases by 1 each time it loops
+        for (n = 0; n <= 9; n++) {
+            //Writes whatever firstLine is, adds a space, and follows it with the next
+            //number in the series by adding the initial input value with the number
+            //of times this has looped
+            String nextLines = firstLine + " " + (rightAngle + n);
             System.out.println(nextLines);
+            //With each new line, "firstLine" is now whatever was just printed. So each
+            //time this loops it will print (whatever it printed in the last line) + " "
+            //+ (rightAngle + n). This way it just keeps adding more numbers each line.
             firstLine = nextLines;
         }
     }
