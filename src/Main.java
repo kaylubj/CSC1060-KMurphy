a063946a229e0053845e662f311dcd185e018a95
Kaylub M <kaylub.murphy.contact@gmail.com>
2025-03-03 08:49:47 -0700

    Just need to add comments

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 Kaylub Murphy
 03/03/2025
 Purpose: Create a program that compares two strings and checks if the user input can be made from the
-random letters that were given.
+random letters that were given
  */
 import java.util.HashMap;
 import java.util.Map;
