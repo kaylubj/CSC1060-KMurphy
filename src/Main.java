071542d86d88cd3fd3a90752cbc703a616be14ce
Kaylub M <kaylub.murphy.contact@gmail.com>
2025-03-03 11:05:04 -0700

    Finished program with comments

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,34 @@ import java.util.Scanner;
 
 public class Main {
 
+    //Checks if the input is a palindrome. Sets the variable that was sent (palindrome)
+    //to yesOrNo
     public static void isPalindrome(String yesOrNo) {
+
+        //Creates a new StringBuilder
         StringBuilder sBuild = new StringBuilder();
 
+        //Turns yesOrNo into a character array called c
+        //Goes through this loop for every character
         for (char c : yesOrNo.toCharArray()) {
+            //Checks if the character is a letter or digit
             if (Character.isLetterOrDigit(c)) {
+                //If it is, it is added to sBuild
                 sBuild.append(c);
             }
         }
 
+        //sBuild.toString() turns the characters contained in sBuild into a string
+        //sBuild.reverse().toString does the same, but reverses the order of the characters
         String forward = sBuild.toString();
         String reversed = sBuild.reverse().toString();
+
+        //Checks if the two strings are equal to each other
         if (forward.equals(reversed)) {
+            //If so it prints this
             System.out.println("This is a palindrome!");
         } else {
+            //Otherwise it prints this
             System.out.println("This is not a palindrome!");
         }
     }
@@ -30,7 +44,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a word: ");
+        //.toLowerCase() sets whatever they typed to lower case
+        //This way, even if the capitalization is different, the program will still
+        //detect if it's a palindrome
         String palindrome = input.nextLine().toLowerCase();
+
+        //Runs the isPalindrome method above, and sends palindrome as the variable
         isPalindrome(palindrome);
     }
 }
