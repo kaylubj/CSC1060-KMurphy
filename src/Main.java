30f3463491024aec3f183b110ce57f8da824303e
Kaylub M <kaylub.murphy.contact@gmail.com>
2025-03-07 08:22:43 -0700

    Just add comments

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        //Creates string ga with the text of the Gettysburg Address
         String ga = "Delivered at the dedication of the Soldiers' National Cemetery at Gettysburg, Pennsylvania.\n" +
                 "\n" +
                 "Four score and seven years ago our fathers brought forth, on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.\n" +
@@ -24,19 +25,37 @@ public class Main {
                 "\n" +
                 "November 19, 1863.";
 
+        //Prints the string
+        System.out.println(ga);
+
+        //Creates a hashmap called numCount1. It calls the numCountMethod and sends
+        //ga to it. Whatever is returned is what the hashmap (numCount2) is set to.
         Map<String,Integer>numCount1 = numCountMethod(ga);
         System.out.println("The times numbers have appeared: \n" + numCount1);
 
+        //Creates a hashmap called letterCount1. It calls the letterCountMethod and sends
+        //ga to it. Whatever is returned is what the hashmap (letterCount2) is set to.
         Map<Character, Integer> letterCount1 = letterCountMethod(ga);
         System.out.println("The number of times each letter has appeared: \n" + letterCount1);
     }
 
+    //This method counts the number of times each number appears in ga. It creates a
+    //Hashmap that associates the numbers in the Gettysburg address with the number of times
+    //That number shows up. The ga string was sent to it when the method was called
     public static Map<String,Integer> numCountMethod(String numOfNums){
+        //Creates a new hashmap called numCount2 (2 to illustrate that it is a separate hashmap
         Map<String,Integer> numCount2 = new HashMap<>();
+
+        //Creates a pattern to match one or more digits. The \d+ makes sure that only
+        //numbers are extracted from the phrase
         Pattern pattern = Pattern.compile("\\d+");
+
+        //The matcher searches the text for numbers
         Matcher matcher = pattern.matcher(numOfNums);
 
+        //matcher.find finds the next number in the text. While a number is found:
         while (matcher.find()){
+            //The number is extracted as a string and added to a group called number
             String number = matcher.group();
             numCount2.put(number, numCount2.getOrDefault(number, 0) +1);
         }
